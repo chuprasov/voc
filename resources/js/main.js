a7679259738317691e945321164878f57e0f599a
chuprasov <125488152+chuprasov@users.noreply.github.com>
2024-04-22 18:09:26 +0300

    Checbox click st2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+
     document.addEventListener("click", function (e) {
         if (e.target.classList.contains("checkbox")) {
-            console.log("*******");
             let getsel = document.querySelector("#get-sel");
             let checkbox = document.querySelectorAll(".checkbox");
             let output = document.querySelector("#trans-string");
             let arr = [];
-
-            console.log("1111111");
-            console.log(checkbox);
-            console.log(output);
 
             for (let i = 0; i < checkbox.length; i++) {
                 if (checkbox[i].checked) {
@@ -26,4 +22,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
             }
         }
     });
+
 });
