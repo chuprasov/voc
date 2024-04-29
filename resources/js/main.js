bd2fd666bda2c4ef93ce35af496b5172ad5a0b02
Maksim Chuprasov <maxchuprasov07@gmail.com>
2024-04-29 18:52:40 +0300

    search with new API

--- resources/js/main.js
+++ resources/js/main.js
@@ -37,62 +37,55 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
     /* Live search with API */
-    const apiUrl = 'https://lara2.fcqdaqp.online/api/products/all';
+    const apiUrl = 'http://localhost:8000/api/search';
+
+    function searchApiByText(query) {
+        const url = `${apiUrl}?text=${encodeURIComponent(query)}`;
     
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(result => {
-            let search = document.querySelector('#sourceText');
-            let searchResults = document.querySelector('#searchResults');
-            let dataArr = result.data;
-            let titleArr = [];
-            for (let i = 0; i < dataArr.length; i++) {
-                titleArr[i] = dataArr[i].title;
-            }
-
-            function updateSearchResults(query) {
-                searchResults.innerHTML = '';
-
-                let filteredWords = titleArr.filter(word => {
-                    return word.toLowerCase().includes(query.toLowerCase());
-                });
-            
-                filteredWords.forEach(word => {
-                    let option = document.createElement('option');
-                    option.classList.add('bg-gray-800', 'text-white')
-                    option.textContent = word;
-                    searchResults.appendChild(option);
-                });
-            }
-
-            search.addEventListener('input', () => {
-                let query = search.value.trim();
-
-                if (search.timer) {
-                    clearTimeout(search.timer);
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
                 }
+                return response.json();
+            })
+            .then(result => {
                 
-                search.timer = setTimeout(() => {
-                    if(search.value !='') {
-                        searchResults.classList.remove('hidden')
-                    }
-                    else {
-                        searchResults.classList.add('hidden')
-                    }
-                    if (query) {
-                        updateSearchResults(query);
-                    } else {
-                        searchResults.innerHTML = '';
-                    }
-                }, 500);
-            });
-        });
-    
-    
+                let searchResults = document.querySelector('#searchResults');
+                let dataArr = result.results;
+                console.log(dataArr)
+                
+                    searchResults.innerHTML = '';
 
+                    let filteredWords = dataArr.filter(word => {
+                        return word.toLowerCase().includes(query.toLowerCase());
+                    });
+                
+                    filteredWords.forEach(word => {
+                        let option = document.createElement('option');
+                        option.classList.add('bg-gray-800', 'text-white')
+                        option.textContent = word;
+                        searchResults.appendChild(option);
+                    });
+            })
+            .catch(error => {
+                    console.error('Fetch error:', error);
+            });
+    }
+    let search = document.querySelector('#sourceText');
+    search.addEventListener('input', () => {
+        let query = search.value.trim();
+
+        if (search.timer) {
+            clearTimeout(search.timer);
+        }
+        
+        search.timer = setTimeout(() => {
+            if (query) {
+                searchApiByText(query);
+            } else {
+                searchResults.innerHTML = '';
+            }
+        }, 500);
+    });
 });
