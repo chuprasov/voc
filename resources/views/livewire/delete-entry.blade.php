7d12dfd453d19093d98dc9509332907c18cfa347
chuprasov <125488152+chuprasov@users.noreply.github.com>
2024-07-24 12:42:23 +0300

    Added ids to dict form and del form

--- resources/views/livewire/delete-entry.blade.php
+++ resources/views/livewire/delete-entry.blade.php
@@ -5,15 +5,14 @@
     </div>
 
     <div class="w-full flex justify-between space-x-4 mt-3">
-        <x-button wire:click="delete" type="button" tabindex="-1" class="w-full !h-16 justify-center">
+        <x-button wire:click="delete" type="button" tabindex="-1" class="w-full !h-16 justify-center" id="btn-yes">
             <div wire:loading wire:target="delete"
                 class="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status">
             </div>
             Yes
         </x-button>
-        <x-button type="button" @click="show = false" tabindex="-1"
-            class="w-full !h-16 justify-center">
+        <x-button type="button" @click="show = false" tabindex="-1" class="w-full !h-16 justify-center" id="btn-no">
             No
         </x-button>
     </div>
